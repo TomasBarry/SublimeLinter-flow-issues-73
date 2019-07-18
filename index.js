f47ce49621cdaf44b8a79a68df4411c45d1c170f
TomasBarry <tomas.barry@hotmail.com>
2019-07-18 14:51:04 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// @flow
+
+import * as React from 'react'
+
+const X = (): React.Fragment => (
+  <React.Fragment>
+    <p>
+      { 'Hello :)' }
+    </p>
+  </React.Fragment>
+)
+
+export default X
